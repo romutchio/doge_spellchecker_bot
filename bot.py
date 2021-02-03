65cc4976c03daed13890f2c709c20ad40cf76f48
romutchio <budljanskiy@mail.ru>
2021-02-04 02:40:45 +0500

    refactoring

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-from config import settings
-
-import telebot
 import random
+
 import language_tool_python
+import telebot
 from telebot.types import Sticker
+
+from config import settings
 
 bot = telebot.TeleBot(settings.TELEGRAM_TOKEN)
 tool = language_tool_python.LanguageToolPublicAPI('ru-RU')
@@ -11,39 +12,40 @@ tool = language_tool_python.LanguageToolPublicAPI('ru-RU')
 sticker_set = bot.get_sticker_set('DoggoStickerms')
 
 if sticker_set:
-	stickers = sticker_set.stickers
+    stickers = sticker_set.stickers
 
 
 def letter_counter(text: str, letter: str = 'м'):
-	return len([x for x in text if x == letter])
+    return len([x for x in text if x == letter])
 
 
 def contains_misspelling(text: str):
-	matches = tool.check(text)
-	for match in matches:
-		if match.replacements and match.ruleIssueType == 'misspelling' \
-				and letter_counter(match.replacements[0]) < letter_counter(text):
-			return True
-	return False
+    matches = tool.check(text)
+    for match in matches:
+        if match.replacements:
+            replacement = match.replacements[0]
+            if match.ruleIssueType == 'misspelling' and letter_counter(replacement) < letter_counter(text):
+                return True
+    return False
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	if contains_misspelling(message.text):
-		if sticker_set:
-			sticker: Sticker = random.choice(sticker_set.stickers)
-			bot.send_sticker(
-				message.chat.id,
-				sticker.file_id,
-				reply_to_message_id=message.message_id,
-			)
-		else:
-			photo = open('doge.jpg', 'rb')
-			bot.send_photo(
-				message.chat.id,
-				photo,
-				reply_to_message_id=message.message_id,
-			)
+    if contains_misspelling(message.text):
+        if sticker_set:
+            sticker: Sticker = random.choice(sticker_set.stickers)
+            bot.send_sticker(
+                message.chat.id,
+                sticker.file_id,
+                reply_to_message_id=message.message_id,
+            )
+        else:
+            photo = open('doge.jpg', 'rb')
+            bot.send_photo(
+                message.chat.id,
+                photo,
+                reply_to_message_id=message.message_id,
+            )
 
 
 bot.polling()
